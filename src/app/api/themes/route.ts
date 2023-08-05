82790c6fc2a11b1052729aaaf315e243429469b8
ls3205 <ls3205@trevor.org>
2023-08-05 09:49:20 -0400

    api/themes route now has querystring functionality to get a specific theme

--- src/app/api/themes/route.ts
+++ src/app/api/themes/route.ts
@@ -1,9 +1,20 @@
+import { NextApiRequest } from "next";
+
 const fs = require('fs')
 
-export async function GET(req: Request) {
+export async function GET(req: NextApiRequest) {
     try {
+        //@ts-ignore
+        const theme = req.nextUrl.searchParams.get(['theme'])
+
         const themes = JSON.parse(fs.readFileSync('./src/components/themes/themes.json'));
-        return new Response(JSON.stringify(themes))
+        var res;
+        if (theme) {
+            res = themes[theme];
+        } else {
+            res = themes
+        }
+        return new Response(JSON.stringify(res))
     } catch (err) {
         console.error(err)
     }
