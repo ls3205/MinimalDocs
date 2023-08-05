2b24fc88945095aed6525d721ad9446a0f0a3b56
ls3205 <ls3205@trevor.org>
2023-08-05 12:04:52 -0400

    bandaiding the query string issue

--- src/app/api/themes/route.ts
+++ src/app/api/themes/route.ts
@@ -2,16 +2,17 @@ const fs = require('fs')
 
 export async function GET(req: Request) {
     try {
-        const { searchParams } = new URL(req.url)
-        const theme = searchParams.get('theme')
+        // const { searchParams } = new URL(req.url)
+        // const theme = searchParams.get('theme')
 
         const themes = JSON.parse(fs.readFileSync('./src/themes/themes.json'));
-        var res;
-        if (theme) {
-            res = themes[theme];
-        } else {
-            res = themes
-        }
+        // var res;
+        // if (theme) {
+        //     res = themes[theme];
+        // } else {
+        //     res = themes
+        // }
+        var res=themes
         return new Response(JSON.stringify(res))
     } catch (err) {
         console.error(err)
