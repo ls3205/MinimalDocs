8dc871a1c8c080ac318b9a4a645a10866516d0aa
ls3205 <ls3205@trevor.org>
2023-08-11 15:28:12 -0400

    completely removed the option to login with google because it is broken

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -11,26 +11,26 @@ import DiscordSVG from "./DiscordSVG";
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: React.FC<UserAuthFormProps> = ({ className, ...props }) => {
-    const [isLoadingGoogle, setIsLoadingGoogle] = useState<boolean>(false)
+    // const [isLoadingGoogle, setIsLoadingGoogle] = useState<boolean>(false)
     const [isLoadingDiscord, setIsLoadingDiscord] = useState<boolean>(false)
     const { toast } = useToast()
 
-    const loginWithGoogle = async () => {
-        setIsLoadingGoogle(true)
+    // const loginWithGoogle = async () => {
+    //     setIsLoadingGoogle(true)
 
-        try {
-            await signIn('google')
-        } catch (err) {
-            toast({
-                title: 'An Error Occurred!',
-                description: 'Sign In Failed',
-                variant: 'destructive',
-                duration: 2000
-            })
-        } finally {
-            setIsLoadingGoogle(false)
-        }
-    }
+    //     try {
+    //         await signIn('google')
+    //     } catch (err) {
+    //         toast({
+    //             title: 'An Error Occurred!',
+    //             description: 'Sign In Failed',
+    //             variant: 'destructive',
+    //             duration: 2000
+    //         })
+    //     } finally {
+    //         setIsLoadingGoogle(false)
+    //     }
+    // }
 
     const loginWithDiscord = async () => {
         setIsLoadingDiscord(true)
@@ -51,10 +51,10 @@ const UserAuthForm: React.FC<UserAuthFormProps> = ({ className, ...props }) => {
 
     return (
         <div className={cn("flex flex-col justify-center", className)} {...props}>
-            <button className="flex flex-row w-auto h-full m-4 p-3 justify-center bg-black rounded-md hover:rounded-xl transition-all duration-300" onClick={loginWithGoogle}>
+            {/* <button className="flex flex-row w-auto h-full m-4 p-3 justify-center bg-black rounded-md hover:rounded-xl transition-all duration-300" onClick={loginWithGoogle}>
                 {isLoadingGoogle ? <Spinner width="w-6" height="h-6" /> : <GoogleSVG />}
                 <p className="text-white ml-4">Google (DO NOT USE, BROKEN)</p>
-            </button>
+            </button> */}
             <button className="flex flex-row w-auto h-full m-4 p-3 justify-center bg-[#5865F2] rounded-md hover:rounded-xl transition-all duration-300" onClick={loginWithDiscord}>
                 {isLoadingDiscord ? <Spinner width="w-6" height="h-6" /> : <DiscordSVG />}
                 <p className="text-white ml-4">Discord</p>
