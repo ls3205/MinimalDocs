7b06e68f6ac28f6f133c63be1e1dfe8af9768d49
ls3205 <ls3205@trevor.org>
2023-08-13 17:15:38 -0400

    attempt #4 of trying to get mobile screens to not be affected by the google navbar

--- src/components/CustomTextArea.tsx
+++ src/components/CustomTextArea.tsx
@@ -121,7 +121,7 @@ function CustomTextArea() {
         <div
             className={`flex absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-4/5 w-1/2 border-[0.25px] border-none rounded-md transition-all duration-300  sm:top-0 sm:left-0 sm:translate-x-0 sm:translate-y-0 sm:border-none sm:w-full sm:h-[92dvh] sm:shadow-none sm:hover:shadow-none`}
         >
-            <ul className="flex flex-col w-full h-full">
+            <ul className="flex flex-col w-full h-full sm:h-[92dvh]">
                 <li className="w-full h-[50px] mb-[1%]">
                     <textarea
                         className={[
@@ -134,7 +134,7 @@ function CustomTextArea() {
                         placeholder={`  enter title...`}
                     />
                 </li>
-                <li className="w-full h-[93%]">
+                <li className="w-full h-full sm:h-max">
                     <textarea
                         className={[
                             `relative transition-all duration-300 border-b-[2px] outline-none w-[95%] h-full left-[2.5%] text-[20px] resize-none pr-[2.5%] bg-bg text-text caret-highlight placeholder-subtext sm:border-none selection:bg-highlight`,
