91d12bd0c0ae952bd641f7ef7dc6a7e2f1f4a248
ls3205 <ls3205@trevor.org>
2024-07-13 14:56:48 +0200

    added privacy policy

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -9,9 +9,9 @@ interface SignInButtonProps {}
 const SignInButton: React.FC<SignInButtonProps> = ({}) => {
     const [isLoading, setIsLoading] = useState<Boolean>(false);
     return (
-        <button className="absolute w-[80px] h-[40px] align-middle justify-center left-[8%] bottom-[7.5%] bg-subtext rounded-lg text-bg hover:bg-text hover:text-subtext active:bg-highlight active:text-highlight transition-all duration-300 active:transition-none sm:hidden">
+        <button className="absolute bottom-[7.5%] left-[8%] h-[40px] w-[80px] justify-center rounded-lg bg-subtext align-middle text-bg transition-all duration-300 hover:bg-text hover:text-subtext active:bg-highlight active:text-highlight active:transition-none sm:hidden">
             <Link
-                className="w-[80px] h-[40px] align-middle justify-center"
+                className="flex h-[40px] w-[80px] flex-row items-center justify-center align-middle"
                 href="/sign-in"
                 onClick={() => setIsLoading(true)}
             >
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
 import { cn } from "@/lib/utils";
 import React, { useState } from "react";
 import GoogleSVG from "./GoogleSVG";
-import { signIn } from 'next-auth/react'
+import { signIn } from "next-auth/react";
 import { useToast } from "./ui/use-toast";
 import Spinner from "./Spinner";
 import DiscordSVG from "./DiscordSVG";
@@ -11,56 +11,73 @@ import DiscordSVG from "./DiscordSVG";
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: React.FC<UserAuthFormProps> = ({ className, ...props }) => {
-    // const [isLoadingGoogle, setIsLoadingGoogle] = useState<boolean>(false)
-    const [isLoadingDiscord, setIsLoadingDiscord] = useState<boolean>(false)
-    const { toast } = useToast()
+	const [isLoadingGoogle, setIsLoadingGoogle] = useState<boolean>(false);
+	const [isLoadingDiscord, setIsLoadingDiscord] = useState<boolean>(false);
+	const { toast } = useToast();
 
-    // const loginWithGoogle = async () => {
-    //     setIsLoadingGoogle(true)
+	const loginWithGoogle = async () => {
+		setIsLoadingGoogle(true);
 
-    //     try {
-    //         await signIn('google')
-    //     } catch (err) {
-    //         toast({
-    //             title: 'An Error Occurred!',
-    //             description: 'Sign In Failed',
-    //             variant: 'destructive',
-    //             duration: 2000
-    //         })
-    //     } finally {
-    //         setIsLoadingGoogle(false)
-    //     }
-    // }
+		try {
+			await signIn("google");
+		} catch (err) {
+			toast({
+				title: "An Error Occurred!",
+				description: "Sign In Failed",
+				variant: "destructive",
+				duration: 2000,
+			});
+		} finally {
+			setIsLoadingGoogle(false);
+		}
+	};
 
-    const loginWithDiscord = async () => {
-        setIsLoadingDiscord(true)
+	const loginWithDiscord = async () => {
+		setIsLoadingDiscord(true);
 
-        try {
-            await signIn('discord')
-        } catch (err) {
-            toast({
-                title: 'An Error Occurred!',
-                description: 'Sign In Failed',
-                variant: 'destructive',
-                duration: 2000
-            })
-        } finally {
-            setIsLoadingDiscord(false)
-        }
-    }
+		try {
+			await signIn("discord");
+		} catch (err) {
+			toast({
+				title: "An Error Occurred!",
+				description: "Sign In Failed",
+				variant: "destructive",
+				duration: 2000,
+			});
+		} finally {
+			setIsLoadingDiscord(false);
+		}
+	};
 
-    return (
-        <div className={cn("flex flex-col justify-center", className)} {...props}>
-            {/* <button className="flex flex-row w-auto h-full m-4 p-3 justify-center bg-black rounded-md hover:rounded-xl transition-all duration-300" onClick={loginWithGoogle}>
-                {isLoadingGoogle ? <Spinner width="w-6" height="h-6" /> : <GoogleSVG />}
-                <p className="text-white ml-4">Google (DO NOT USE, BROKEN)</p>
-            </button> */}
-            <button className="flex flex-row w-auto h-full m-4 p-3 justify-center bg-[#5865F2] rounded-md hover:rounded-xl transition-all duration-300" onClick={loginWithDiscord}>
-                {isLoadingDiscord ? <Spinner width="w-6" height="h-6" /> : <DiscordSVG />}
-                <p className="text-white ml-4">Discord</p>
-            </button>
-        </div>
-    );
+	return (
+		<div
+			className={cn("flex flex-col justify-center", className)}
+			{...props}
+		>
+			<button
+				className="flex flex-row w-auto h-full m-4 mb-2 p-3 justify-center bg-black rounded-md hover:rounded-xl transition-all duration-300"
+				onClick={loginWithGoogle}
+			>
+				{isLoadingGoogle ? (
+					<Spinner width="w-6" height="h-6" />
+				) : (
+					<GoogleSVG />
+				)}
+				<p className="text-white ml-4">Google</p>
+			</button>
+			<button
+				className="flex flex-row w-auto h-full m-4 mt-2 p-3 justify-center bg-[#5865F2] rounded-md hover:rounded-xl transition-all duration-300"
+				onClick={loginWithDiscord}
+			>
+				{isLoadingDiscord ? (
+					<Spinner width="w-6" height="h-6" />
+				) : (
+					<DiscordSVG />
+				)}
+				<p className="text-white ml-4">Discord</p>
+			</button>
+		</div>
+	);
 };
 
 export default UserAuthForm;
